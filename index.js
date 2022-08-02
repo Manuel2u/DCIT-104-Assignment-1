79ffb8e571ab1f2f3b5d279c5847d280a168ddbf
Manuel2u <edodoo011@st.ug.edu.gh>
2022-08-02 16:15:18 +0000

    Added student ID and name

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+// Written by Emmanuel Nii Dodoo aka Black Elon Musk.
+// student ID : 10945448
+
 var num = Number(prompt("Enter your number: "));
 var prime_num = [];
 
-function isPrime(num){
-    
-    if (num <= 1)
-        return false;
-    
+function isPrime(num) {
 
-    for (let i = 2; i < num; i++)
-        if (num % i === 0)
-            return false;
-        
-    return true;
-    
+  if (num <= 1)
+    return false;
+
+
+  for (let i = 2; i < num; i++)
+    if (num % i === 0)
+      return false;
+
+  return true;
+
 }
 
-function printPrime(num){
-    for (let i = 2; i <= num; i++) {
-        if (isPrime(i)){
-            prime_num.push(i)
-        }     
-}
+function printPrime(num) {
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      prime_num.push(i)
+    }
+  }
 }
 
 printPrime(num)
@@ -28,10 +31,8 @@ printPrime(num)
 var num_of_prime = prime_num.length;
 var sum = 0;
 
-for(i=0 ; i < num_of_prime ; i++ ){
-     sum += prime_num[i]
+for (i = 0; i < num_of_prime; i++) {
+  sum += prime_num[i]
 }
 
 console.log(sum);
-
-
